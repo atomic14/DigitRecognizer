3504e1e68d40b9405a125ec17dabd8e59705681b
Chris Greening <chris@cmgresearch.com>
2017-06-25 19:11:46 +0100

    Initial Commit

---
+++ digits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  digits
+//
+//  Created by Chris Greening on 25/06/2017.
+//  Copyright © 2017 Chris Greening. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
